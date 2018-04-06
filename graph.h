00faeb24949af2ad281beec8924405938bd8772e
leoniesorcelle <leonie.sorcelle@hotmail.fr>
2018-04-06 10:17:51 +0200

    V3 Leonie

--- graph.h
+++ graph.h
@@ -1,83 +1,10 @@
 #ifndef GRAPH_H_INCLUDED
 #define GRAPH_H_INCLUDED
 
-/**************************************************************
-    Ici sont propos�es 3 classes fondamentales
-            Vertex (=Sommet)
-            Edge (=Ar�te ou Arc)
-            Graph (=Graphe)
-
-    Les ar�tes et les sommets et le graphe qu'ils constituent
-    "travaillent" �troitement ensemble : pour cette raison les
-    Vertex et Edge se d�clarent amis (friend) de Graph pour que
-    ce dernier puisse librement acc�der aux membres (y compris
-    protected ou private) de Vertex et Edge.
-
-    Ces Classes peuvent �tres compl�t�es. Il est �galement possible
-    de les d�river mais il est malheureusement assez difficile
-    de d�river le "triplet" des 3 classes en maintenant des relations
-    coh�rentes ( rechercher "c++ class covariance" et "c++ parallel inheritance"
-    pour commencer .. ). Il est donc sans doute pr�f�rable, si possible,
-    de "customiser" ces classes de base directement, sans h�ritage.
-
-    Le mod�le propos� permet de repr�senter un graphe orient� �ventuellement
-    pond�r�, les arcs portent une ou des informations suppl�mentaire(s).
-    Les relations/navigations Arcs -> Sommets et Sommets -> Arcs se font
-    dans les 2 sens et utilisent des INDICES et NON PAS DES ADRESSES (pointeurs)
-    de telle sorte que la topologie du graphe puisse �tre assez facilement
-    lue et �crite en fichier, et b�n�ficie d'une bonne lisibilit� en cas de bugs...
-
-    Chaque arc poss�de 2 attributs principaux (en plus de son �ventuelle pond�ration)
-        -> m_from (indice du sommet de d�part de l'arc )
-        -> m_to (indice du sommet d'arriv�e de l'arc )
-
-    Chaque sommet poss�de 2 liste d'arcs (en plus de ses attributs "internes", marquages...)
-        -> m_in (liste des indices des arcs arrivant au sommet : acc�s aux pr�d�cesseurs)
-        -> m_out (liste des indices des arcs partant du sommet : acc�s aux successeurs)
-
-    Cependant le probl�me des indices (par rapport aux pointeurs) et qu'en cas
-    de destruction d'une entit� (un arc et/ou un sommet sont enlev�s du graphe) alors :
-
-    - Soit il faut reprendre toute la num�rotation pour "boucher le trou"
-      (par exemple on a supprim� le sommet n�4, le sommet n�5 devient le 4, 6 devient 5 etc...)
-      ce qui pose des probl�mes de stabilit� et de coh�rence, et une difficult� � r�-introduire
-      le(s) m�me(s) �l�ment supprim� (au m�me indice)
-
-    - Soit on admet que la num�rotation des sommets et arcs n'est pas contigue, c�d qu'il
-      peut y avoir des "trous" : sommets 0 1 5 7 8, pas de sommets 2 ni 3 ni 4 ni 6. La num�rotation
-      est stable mais on ne peut plus utiliser un simple vecteur pour ranger la liste de tous
-      les arcs et tous les sommets aux indices correspondants, on peut utiliser une map
-      qui associe un objet arc ou sommet � des indices arbitraires (pas forc�ment contigus)
-
-    C'est cette 2�me approche qui est propos�e ici : dans la classe graphe vous trouverez
-        -> map<int, Edge>   m_edges
-        -> map<int, Vertex> m_vertices    (le pluriel de vertex est vertices)
-
-    Il faudra �tre attentif au fait que par rapport � un simple vecteur, le parcours des �l�ments
-    ne pourra PAS se faire avec un simple for (int i=0; i<m_edges.size(); ++i) ...m_edges[i]...
-    et que les parcours � it�rateur ne donneront pas directement des Edge ou des Vertex
-    mais des pairs, l'objet d'int�r�t se trouvant dans "second" ("first" contenant l'indice)
-                for (auto &it = m_edges.begin(); it!=m_edges.end(); ++it) ...it->second...
-    ou bien     for (auto &e : m_edges) ...e.second...
-
-    Il n'est pas obligatoire d'utiliser ces classes pour le projet, vous pouvez faire les votres
-
-    Au niveau de l'interface, on dissocie une classe interface associ�e � chaque classe fondamentale
-    de telle sorte qu'il soit possible de travailler avec des graphes non repr�sent�s � l'�cran
-    Imaginons par exemple qu'on doive g�n�rer 1000 permutations de graphes pour tester des
-    combinaisons, on ne souhaite pas repr�senter graphiquement ces 1000 graphes, et les
-    interfaces p�sent lourd en ressource, avec cette organisation on est libre de r�server ou
-    pas une interface de pr�sentation associ�e aux datas (d�couplage donn�es/interface)
-
-***********************************************************************************************/
-
 #include <vector>
 #include <map>
 #include <string>
 #include <memory>
-
-#include <iostream>
-#include <stack>
 #include "grman/grman.h"
 
 /***************************************************
@@ -125,24 +52,19 @@ class Vertex
     friend class Edge;
     friend class EdgeInterface;
 
-    private :
+    public :
         /// liste des indices des arcs arrivant au sommet : acc�s aux pr�d�cesseurs
         std::vector<int> m_in; // plusieurs fleche qui arrive
         /// liste des indices des arcs partant du sommet : acc�s aux successeurs
         std::vector<int> m_out; // plusieurs fleche qui parte
         /// un exemple de donn�e associ�e � l'arc, on peut en ajouter d'autres...
-        double m_value; // poids
-        //std::string m_nom;
-
+        double m_value; // poid
         int m_id; // identifiant
-        int m_x,m_Y; //coordonnées
+        int m_x,m_Y; //coordonnee
         /// le POINTEUR sur l'interface associ�e, nullptr -> pas d'interface
         std::shared_ptr<VertexInterface> m_interface = nullptr;
-        // Docu shared_ptr : https://msdn.microsoft.com/fr-fr/library/hh279669.aspx
-        // La ligne pr�c�dente est en gros �quivalent � la ligne suivante :
-        // VertexInterface * m_interface = nullptr;
 
-    public :
+
         /// Les constructeurs sont � compl�ter selon vos besoin...
         /// Ici on ne donne qu'un seul constructeur qui peut utiliser une interface
         Vertex (double _value=0,VertexInterface *interface=nullptr) :
@@ -196,15 +118,18 @@ class Edge
     private :
         /// indice du sommet de d�part de l'arc
         int m_from;
+
         /// indice du sommet d'arriv�e de l'arc
         int m_to;
+
         /// un exemple de donn�e associ�e � l'arc, on peut en ajouter d'autres...
-        double m_weight; //poids
-        int m_i; // identifiant
+        double m_weight; //poid
+
         /// le POINTEUR sur l'interface associ�e, nullptr -> pas d'interface
         std::shared_ptr<EdgeInterface> m_interface = nullptr;
 
-    public :
+    public:
+
         /// Les constructeurs sont � compl�ter selon vos besoin...
         /// Ici on ne donne qu'un seul constructeur qui peut utiliser une interface
         Edge (double weight=0, EdgeInterface *interface=nullptr) :
@@ -238,49 +163,30 @@ class GraphInterface
         // A compl�ter �ventuellement par des widgets de d�coration ou
         // d'�dition (boutons ajouter/enlever ...)
 
-        grman::WidgetButton m_boutonAjout;
-        grman::WidgetImage m_boutonAjout_image;
-        grman::WidgetButton m_boutonSupp;
-        grman::WidgetImage m_boutonSupp_image;
-        grman::WidgetButton m_boutonCharger;
-        grman::WidgetImage m_boutonCharger_image;
-        grman::WidgetButton m_boutonSauvegarder;
-        grman::WidgetImage m_boutonSauvegarder_image;
-        grman::WidgetButton m_boutonQuitter;
-        grman::WidgetImage m_boutonQuitter_image;
-        grman::WidgetButton m_boutonAlgoChoix;
-        grman::WidgetImage m_boutonAlgoChoix_image;
-        grman::WidgetText m_algo;
-        grman::WidgetText m_algo2_1;
-        grman::WidgetText m_algo2_2;
-        grman::WidgetText m_algo2_3;
-        grman::WidgetText m_algo2_4;
-
     public :
         // Le constructeur met en place les �l�ments de l'interface
         // voir l'impl�mentation dans le .cpp
         GraphInterface(int x, int y, int w, int h);
-        //void update();
 };
 
 class Graph
 {
-    friend class Menu;
-
-    private :
+    public :
         /// La "liste" des ar�tes
         std::map<int, Edge> m_edges; //indice et arrete
+
         /// La liste des sommets
         std::map<int, Vertex> m_vertices;
 
         int m_ordre;
+
         int m_arete;
 
         /// le POINTEUR sur l'interface associ�e, nullptr -> pas d'interface
         std::shared_ptr<GraphInterface> m_interface = nullptr;
 
-    public :
-        /// Les constructeurs sont � compl�ter selon vos besoins...
+
+        /// Les constructeurs sont � compl�ter selon vos besoin...
         /// Ici on ne donne qu'un seul constructeur qui peut utiliser une interface
         Graph (GraphInterface *interface=nullptr) :
             m_interface(interface)  {  }
@@ -292,7 +198,7 @@ class Graph
         /// Voir impl�mentation dans le .cpp
         /// Cette m�thode est � enlever et remplacer par un syst�me
         /// de chargement de fichiers par exemple.
-        //void make_example();
+        void make_example();
 
         /// La m�thode update � appeler dans la boucle de jeu pour les graphes avec interface
         void update();
@@ -305,41 +211,25 @@ class Graph
         void rajout1();
         void rajout2();
         void rajout3();
-        void supprimer_edge(int edx);
+        void reglage1();
+        void reglage2();
+        void reglage3();
+        void Supprimer_edge(int edx);
         void supprimer_Sommet(int idx);
-};
 
-class Menu
-{
-    friend class Graph;
-
-    private :
-    /// Les widgets de l'interface. N'oubliez pas qu'il ne suffit pas de d�clarer
-    /// ici un widget pour qu'il apparaisse, il faut aussi le mettre en place et
-    /// le param�trer ( voir l'impl�mentation du constructeur dans le .cpp )
-
-    /// La boite qui contient toute l'interface d'un graphe
-    grman::WidgetBox m_top_box;
-    /// Dans cette boite seront ajout�s les (interfaces des) sommets et des arcs...
-    grman::WidgetBox m_main_box;
-    /// Dans cette boite seront ajout�s des boutons de contr�le etc...
-    grman::WidgetBox m_tool_box;
-    // A compl�ter �ventuellement par des widgets de d�coration ou
-    // d'�dition (boutons ajouter/enlever ...)
-
-    grman::WidgetBox m_Menu;
-    grman::WidgetImage m_Menu_image;
-    grman::WidgetButton m_Reseau1;
-    grman::WidgetButton m_Reseau2;
-    grman::WidgetButton m_Reseau3;
-    grman::WidgetButton m_MenuQuitter;
-
-    std::shared_ptr<GraphInterface> m_interface = nullptr;
-    // std::shared_ptr<Graph> m_recuperation = nullptr;
-
-    public :
-      Menu();
-      void update(Graph g);
+        void reglage_2moins_1plus(int i,int k,int l,int j, double p,double v,double w);
+        void reglage_2moins_1plus_p(int i,int k,int l,int j, double p,double v,double w);
+        void reglage_2plus_1moins(int i,int k,int l,int j, double p,double v,double w);
+        void reglage_2plus_1moins_p(int i,int k,int l,int j, double p,double v,double w);
+        void reglage_2plus_2moins(int i, int k,int l,int m, int j, double p, double v,double w, double z);
+        void reglage_div(int i,int j, double v); // influence positive
+        void reglage_div_moins(int i,int j, double v); // influence negative
+        void reglage_div_p(int i,int k,int j, double p,double v); // 2 influence positive
+        void reglage_moins_plus(int i,int k,int j, double p,double v); // 2 influence negative
+        void reglage_multi(int i,int j, double v); //influence positive
+        void reglage_multi_moins(int i,int j, double v);// influence negative
+        void reglage_multi_p(int i,int k,int j,double p, double v);// 2 influence positive
+        void reglage_plus_moins(int i,int k,int j,double p, double v); // 2 influence negative
 };
 
 #endif // GRAPH_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include "grman/grman.h"
 #include <iostream>
+
 #include "graph.h"
-
 using namespace std;
-
 int main()
 {
     /// A appeler en 1er avant d'instancier des objets graphiques etc...
@@ -11,25 +10,28 @@ int main()
     /// Le nom du r�pertoire o� se trouvent les images � charger
     grman::set_pictures_path("pics");
     /// Un exemple de graphe
-    Menu m;
     Graph g;
-    //g.menu();
-    //g.recuperation1();
-    //g.rajout1();
-    //g.supprimer_Sommet();
+   // g.menu();
+    g.recuperation2();
+
+   // g.rajout1();
+ //g.supprimer_Sommet(4);
 
     /// Vous gardez la main sur la "boucle de jeu"
     /// ( contrairement � des frameworks plus avanc�s )
     while ( !key[KEY_ESC] )
     {
-        m.update(g);
         /// Il faut appeler les m�thodes d'update des objets qui comportent des widgets
-        //g.update();
+        g.update();
+
+   // g.reglage2();
+
         /// Mise � jour g�n�rale (clavier/souris/buffer etc...)
         grman::mettre_a_jour();
+
     }
 
-    g.sauvegarde1();
+g.sauvegarde2();
     grman::fermer_allegro();
 
     return 0;
